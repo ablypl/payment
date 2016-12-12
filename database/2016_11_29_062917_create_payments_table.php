283c8786634de6e9d8c9f9e29cdbeada317d3057
seszcze <sebastian@ably.pl>
2016-12-12 09:48:34 +0100

    Update db schema

--- database/2016_11_29_062917_create_payments_table.php
+++ database/2016_11_29_062917_create_payments_table.php
@@ -1,40 +1,37 @@
-e Illuminate\Support\Facades\Schema;
+<?php
+
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
     {
-    /**
-     *      * Run the migrations.
-     *           *
-     *                * @return void
-     *                     */
-            public function up()
-                    {
-                        Schema::create('payments', function (Blueprint $table) {
-                                        $table->increments('id');
-                                                    $table->integer('amount')->nullable();
-                                                    $table->string('service');
-                                                                $table->string('service_id')->nullable();
-                                                                $table->date('paid_at')->nullable();
-                                                                            $table->integer('payable_id')->unsigned()->index()->nullable();
-                                                                            $table->string('payable_type')->nullable();
-                                                                                        $table->timestamps();
-                                                                                    
-});
-    
-}
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('amount')->nullable();
+            $table->string('service');
+            $table->string('service_id')->nullable();
+            $table->date('paid_at')->nullable();
+            $table->integer('payable_id')->unsigned()->index()->nullable();
+            $table->string('payable_type')->nullable();
+            $table->timestamps();
+        });
+    }
 
     /**
-     *      * Reverse the migrations.
-     *           *
-     *                * @return void
-     *                     */
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
-            {
+    {
         Schema::dropIfExists('payments');
-    
+    }
 }
-
-}
-
